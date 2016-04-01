ae6892e712f19eefb9a70655bdd5c6b01a951e50
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-03-31 21:21:17 -0700

    cleanup / package check

--- R/mlb_database.R
+++ R/mlb_database.R
@@ -1,20 +1,26 @@
 #' @title Get MLB data from 2015 to the present
-#' @name database_init
+#' @name gameday_database_init
 #' @description Scrapes the GameDay database and saves output to an SQLite database
-#' @export database_init
+#' @export gameday_database_init
 #' @param sqlite_db_file a place to write the SQLite database
 #' @return NULL
 
-database_init <- function(sqlite_db_file) {
+gameday_database_init <- function(sqlite_db_file) {
   connection <- dplyr::src_sqlite(sqlite_db_file, create = TRUE)
-  suffix_all <-  c(
-    "players.xml",
-    "miniscoreboard.xml",
-    "inning/inning_all.xml",
-    "inning/inning_hit.xml")
   pitchRx::scrape(
     start = "2015-02-01",
-    end = Sys.Date(),
-    connect = connection$con,
-    suffix = suffix_all)
+    end = Sys.Date() - 1,
+    connect = connection$con)
+}
+
+#' @title Update GameDay database
+#' @name gameday_database_update
+#' @description updates the GameDay database
+#' @export gameday_database_update
+#' @param sqlite_db_file the SQLite database file to update
+#' @return NULL
+
+gameday_database_update <- function(sqlite_db_file) {
+  connection <- dplyr::src_sqlite(sqlite_db_file, create = FALSE)
+  pitchRx::update_db(connect = connection$con)
 }
